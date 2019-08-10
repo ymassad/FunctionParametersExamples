7933add8cb1ea7754e653a32c23351c2749444f1
Yacoub Massad <yacoubmassad@gmail.com>
2019-08-10 19:10:02 +0300

    Adding Tool6

---
+++ Tool6/ComplaintsReportingSettings.cs
@@ -0,0 +1,34 @@
+﻿namespace Tool6
+{
+    public abstract class ComplaintsReportingSettings
+    {
+        private ComplaintsReportingSettings()
+        {
+        }
+
+        public sealed class DoNotGenerate : ComplaintsReportingSettings
+        {
+
+        }
+
+        public sealed class Generate : ComplaintsReportingSettings
+        {
+            public Generate(bool includeOnlyOpenCustomerComplaints)
+            {
+                IncludeOnlyOpenCustomerComplaints = includeOnlyOpenCustomerComplaints;
+            }
+
+            public bool IncludeOnlyOpenCustomerComplaints { get; }
+        }
+
+        public sealed class GenerateOnlySummary : ComplaintsReportingSettings
+        {
+            public GenerateOnlySummary(bool includeOnlyOpenCustomerComplaintsForSummary)
+            {
+                IncludeOnlyOpenCustomerComplaintsForSummary = includeOnlyOpenCustomerComplaintsForSummary;
+            }
+
+            public bool IncludeOnlyOpenCustomerComplaintsForSummary { get; }
+        }
+    }
+}
---
+++ Tool6/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+
+namespace Tool6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ValidateInputs())
+                return;
+
+            var customerId = int.Parse(CustomerIdTextBox.Text);
+
+            var outputFilename = OutputFilenameTextBox.Text;
+
+            ComplaintsReportingSettings complaintsReportingSettings;
+
+            if (DoNotIncludeCustomerComplaintsRadioButton.IsChecked == true)
+            {
+                complaintsReportingSettings = new ComplaintsReportingSettings.DoNotGenerate();
+            }
+            else if (IncludeCustomerComplaintsRadioButton.IsChecked == true)
+            {
+                complaintsReportingSettings = new ComplaintsReportingSettings.Generate(
+                    IncludeOnlyOpenComplaintsCheckBox.IsChecked == true);
+            }
+            else// if (IncludeOnlyASummaryOfCustomerComplaintsRadioButton.IsChecked == true)
+            {
+                complaintsReportingSettings = new ComplaintsReportingSettings.GenerateOnlySummary(
+                    IncludeOnlyOpenComplaintsForSummaryCheckBox.IsChecked == true);
+            }
+
+            GenerateReport(
+                customerId,
+                outputFilename,
+                complaintsReportingSettings);
+        }
+
+        private bool ValidateInputs()
+        {
+            //TODO: validate inputs
+            return true;
+        }
+
+        void GenerateReport(
+            int customerId,
+            string outputFilename,
+            ComplaintsReportingSettings complaintsReportingSettings)
+        {
+            //TODO: generate report
+        }
+    }
+}
