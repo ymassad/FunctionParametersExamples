e934c641d3f0b2b7f13886b4c48f836dfb98d169
Yacoub Massad <yacoubmassad@gmail.com>
2019-08-10 17:54:34 +0300

    Adding Tool5

---
+++ Tool5/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+
+namespace Tool5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ValidateInputs())
+                return;
+
+            var customerId = int.Parse(CustomerIdTextBox.Text);
+
+            var outputFilename = OutputFilenameTextBox.Text;
+
+            var includeCustomerComplaints = IncludeCustomerComplaintsRadioButton.IsChecked == true;
+
+            var includeOnlyOpenCustomerComplaints = IncludeOnlyOpenComplaintsCheckBox.IsChecked == true;
+
+            var includeOnlyASummaryOfCustomerComplaints = IncludeOnlyASummaryOfCustomerComplaintsRadioButton.IsChecked == true;
+
+            var includeOnlyOpenCustomerComplaintsForSummary = IncludeOnlyOpenComplaintsForSummaryCheckBox.IsChecked == true;
+
+            GenerateReport(
+                customerId,
+                outputFilename,
+                includeCustomerComplaints,
+                includeOnlyOpenCustomerComplaints || includeOnlyOpenCustomerComplaintsForSummary,
+                includeOnlyASummaryOfCustomerComplaints);
+        }
+
+        private bool ValidateInputs()
+        {
+            //TODO: validate inputs
+            return true;
+        }
+
+        void GenerateReport(
+            int customerId,
+            string outputFilename,
+            bool includeCustomerComplaints,
+            bool includeOnlyOpenCustomerComplaints,
+            bool includeOnlyASummaryOfCustomerComplaints)
+        {
+            //TODO: generate report
+        }
+    }
+}
