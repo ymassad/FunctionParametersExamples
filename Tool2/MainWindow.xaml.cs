d67a04f0a8553892fb1849f38e09d13bc54bfd7d
Yacoub Massad <yacoubmassad@gmail.com>
2019-08-10 13:16:04 +0300

    Adding Tool2

---
+++ Tool2/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace Tool2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GenerateReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!ValidateInputs())
+                return;
+
+            var customerId = int.Parse(CustomerIdTextBox.Text);
+
+            var outputFilename = OutputFilenameTextBox.Text;
+
+            var includeCustomerComplaints = IncludeCustomerComplaintsCheckBox.IsChecked == true;
+
+            GenerateReport(customerId, outputFilename, includeCustomerComplaints);
+        }
+
+        private bool ValidateInputs()
+        {
+            //TODO: validate inputs
+            return true;
+        }
+
+        public static void GenerateReport(int customerId, string outputFilename, bool includeCustomerComplaints)
+        {
+            //TODO: generate report
+        }
+    }
+}
